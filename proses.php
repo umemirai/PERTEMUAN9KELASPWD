69eec771c5f67ad26d4e8941ce1f2d56d971f4a1
kikipangetsu <jekaymputh00@gmail.com>
2025-04-24 12:50:21 +0700

    mengedit proses.php menambahkan alert

--- proses.php
+++ proses.php
@@ -2,9 +2,17 @@
 
     require_once('Biodata.php');
 
+    if($_SERVER['REQUEST_METHOD']=='post'){
     $nama = $_POST['nama'];
     $umur = $_POST['umur'];
     $email = $_POST['email'];
 
     $biodata = new Biodata($nama, $umur, $email);
     $biodata->infoBiodata();
+    }else{
+        echo "<script>alert('Anda tidak berhak akses halaman ini')</script>";
+        ?>
+        <meta http-equiv="refresh" content="0; url=form.php">
+        <?php
+    }
+    
